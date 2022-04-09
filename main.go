047bb1e571a0fb2e9b737b54a9f693dd73eafec6
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-04-09 14:05:10 -0300

    feat: add connection with mongodb

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/Uemerson/keep-safe-go/keep"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	log.Println("connected to mongodb!")
 	r := mux.NewRouter()
 	keep.Handler(r)
 	log.Fatal(http.ListenAndServe(":8000", r))
