8114b965f737eb17ce5f8e191aad5d185fb08cc0
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-03-17 17:19:24 -0300

    feat: add method getnotes to note repository

--- internal/repository/note_repository.go
+++ internal/repository/note_repository.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"context"
+	"time"
 
 	"github.com/Uemerson/keep-safe-go/internal/entity"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -19,4 +21,28 @@ func (nr *NoteRepository) SaveNote(note *entity.NoteEntity) (*mongo.InsertOneRes
 	notesCollection := nr.db.Database("keepsafe").Collection("notes")
 	result, err := notesCollection.InsertOne(context.TODO(), note)
 	return result, err
+}
+
+func (nr *NoteRepository) GetNotes() ([]*entity.NoteEntity, error) {
+	notesCollection := nr.db.Database("keepsafe").Collection("notes")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	cur, err := notesCollection.Find(ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+	defer cur.Close(ctx)
+	var results []*entity.NoteEntity
+	for cur.Next(ctx) {
+		var result *entity.NoteEntity
+		err := cur.Decode(&result)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, result)
+	}
+	if err := cur.Err(); err != nil {
+		return nil, err
+	}
+	return results, nil
 }
